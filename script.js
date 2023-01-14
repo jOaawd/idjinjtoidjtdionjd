4bd5fe0f366e3d1e7e34f95972b156c3b23a2ec9
shufu420 <122318862+shufu420@users.noreply.github.com>
2023-01-13 22:07:11 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+function sex () {
+    if (window.confirm('SHIMMER SEX??????'))
+{
+    alert('SHIMMER SEX!!!!!!!!!!!!!');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
+    window.open('https://e621.net/', '_blank');
+}
+else
+{
+    die();
+}
+} 
