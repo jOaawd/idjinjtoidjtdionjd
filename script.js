407948e85919851c3e73d81208e102a15f658809
shufu420 <122318862+shufu420@users.noreply.github.com>
2023-01-13 22:19:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function sex () {
     window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
     window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
     window.open('https://www.etsy.com/market/shimmer_sex', '_blank');
-    window.open('https://e621.net/', '_blank');
     window.open('https://sh1mmer.glitch.me', '_blank');
 }
 else
